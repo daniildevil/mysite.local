fa8cad794ae6489db459818affd75b54a08fe19e
daniildevil <barsadn@gmail.com>
2020-11-29 01:50:17 +0200

    3 task

--- lesson2.php
+++ lesson2.php
@@ -1,15 +1,14 @@
-
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="stylesheets.css">
-<title>mysite.local</title>
+
+    <title>mysite.local</title>
 </head>
 <body>
 
 <header>
     <h1>Welcome mysite.local!</h1>
     <nav>
-
         <a href="index.php">На главную</a>
         <a href="https://github.com/daniildevil/mysite.local/tree/master">My GitHub</a>
         <a href="lesson1.php">Урок 1</a>
@@ -17,15 +16,14 @@
     </nav>
 </header>
 <main>
-    <section>
+    <div>
         <?php
-        echo "Hello,<b>NIX Education!</b>";
+        echo "COLOR";
+
         ?>
-    </section>
-
-    <section>I`m going to be web-developer</section>
-
+    </div>
 </main>
+
 <footer>
     Сайт сделан сегодня и все права принадлежат его создателю :)
 </footer>
